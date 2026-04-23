399820009985c3c1980696316cd07b2740795ef6
madnessman666 <mihamiha19@outlook.com>
2026-04-23 16:18:03 +0300

    Fix idiotizm (types of hooks)

--- src/collections/Users/hooks.ts
+++ src/collections/Users/hooks.ts
@@ -1,7 +1,6 @@
-import { BeforeValidateHook } from "payload/dist/globals/config/types"
-import { FieldHook } from "payload/types"
+import { CollectionBeforeValidateHook } from "payload/types"
 
-export const EmailCheck: BeforeValidateHook = async ({originalDoc, data}) => {
+export const EmailCheck: CollectionBeforeValidateHook = async ({originalDoc, data}) => {
     var regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     if(!regEx.test(data.email)){
         throw new Error("Введите пожалуйста правильную почту!")
